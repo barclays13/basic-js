895c02cde992d81ce3166dc03287ac10d2765a16
Sergey <barclays1313@gmail.com>
2021-09-19 15:02:10 +0300

    feat: implement extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ export default  function repeater( str, options) {
+
+  str = String(str);
+  let {repeatTimes = 1, 
+    separator= "+", 
+    addition = "", 
+    additionRepeatTimes = 1,
+    additionSeparator = "|"} = options;
+
+  repeatTimes = (typeof repeatTimes === "number") ? repeatTimes : +(repeatTimes);
+  separator = (typeof separator === "string") ? separator : String(separator);
+  addition = (typeof addition === "string") ? addition : String(addition);   
+  additionRepeatTimes = (typeof additionRepeatTimes === "number") ? additionRepeatTimes : +(additionRepeatTimes); 
+  additionSeparator = (typeof additionSeparator === "string") ? additionSeparator : String(additionSeparator);
+
+  const getString = (string, repeat, separator) => {
+    if (!repeat) return ""; 
+    let result = ``;
+    for(let i = 1; i <= repeat; i++) {
+       i < repeat ? result += `${string}${separator}`: result += string;
+    }
+    return result;
+  }
+
+  return getString(str + getString(addition, additionRepeatTimes, additionSeparator), repeatTimes, separator);
 }
