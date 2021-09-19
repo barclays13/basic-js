91d9f49daf893760d5fcb78e1e7c95eebf1d9db1
Sergey <barclays1313@gmail.com>
2021-09-19 20:29:13 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,13 +19,79 @@ import { NotImplementedError } from '../extensions/index.js';
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+ const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct
+    return this.direct
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    message = message.toUpperCase();    
+    key = key.toUpperCase();
+    let count = 0,
+      result = "";
+  
+    message.split("").forEach(element => {
+      const indexElement = alphabet.indexOf(element);
+
+      if (indexElement == -1) {
+        result += element;
+      } else {
+        const letterKey = key[count % key.length];
+        const indexKey = alphabet.indexOf(letterKey);
+        let  number = indexElement + indexKey >= 26 ? (indexElement + indexKey - 26) : (indexElement + indexKey);
+        count += 1;
+        result += alphabet[number];
+      }
+    });
+    
+    if (!this.direct) {
+      return result
+      .split("")
+      .reverse()
+      .join("")
+    } else {
+      return result
+    }
+
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    message = message.toUpperCase();    
+    key = key.toUpperCase();
+    let count = 0;
+    let result = "";
+
+    message.split("").forEach(element => {
+      const indexElement = alphabet.indexOf(element);
+
+      if (indexElement == -1) {
+        result += element;
+      } else {
+        const letterKey = key[count % key.length];
+        const indexKey = alphabet.indexOf(letterKey);
+        let number = indexElement < indexKey ? (26 - indexKey + indexElement) : indexElement - indexKey;
+        count += 1;
+        result += alphabet[number];
+      }
+    });
+
+    if (!this.direct) {
+      return result
+      .split("")
+      .reverse()
+      .join("")
+    } else {
+      return result
+    }
   }
 }
